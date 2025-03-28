e95be743374a7d465a829f77c5eb24abe9f7258a
JohnDev <andreinicolas.codebank@gmail.com>
2025-03-28 20:06:23 +0800

    Configure application to listen on specific URLs for HTTP and HTTPS

--- Sample-AutoDeploy/Program.cs
+++ Sample-AutoDeploy/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:7258");
 
 var app = builder.Build();
 
